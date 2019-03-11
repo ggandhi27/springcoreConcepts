2f7ee0ba338d8513ac6d05200ad26841fad92ec9
Gaurav Gandhi <gauravgo4friends@gmail.com>
2019-03-11 12:16:01 +0530

    Created constructor-arg and property tags with the restaurant class

--- SpringCoreBootcamp/src/main/java/com/basic/example/Restaurant.java
+++ SpringCoreBootcamp/src/main/java/com/basic/example/Restaurant.java
@@ -5,6 +5,50 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Restaurant {
     
+    private Drink drink;
+    private int age;
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public Drink getDrink() {
+        return drink;
+    }
+    
+    public void setDrink(Drink drink) {
+        this.drink = drink;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public Restaurant(Drink drink, String name) {
+        this.drink = drink;
+        this.name = name;
+    }
+    
+    private String name;
+    
+    
+    @Override
+    public String toString() {
+        return "Restaurant{" +
+                "drink=" + drink +
+                ", age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+    
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
         
@@ -12,5 +56,8 @@ public class Restaurant {
         drink.getDrink();
         drink = applicationContext.getBean("tea2",Tea.class);
         drink.getDrink();
+        
+        Restaurant restaurant = (Restaurant) applicationContext.getBean("restaurant");
+        System.out.println(restaurant);
     }
 }
